ccecdc39824facdcff6230f5216ab8f29938d059
dakshcodez <daksharoracodez@gmail.com>
2025-06-09 19:25:39 +0530

    made progress

--- in C++/bst/basic_ops.cpp
+++ in C++/bst/basic_ops.cpp
@@ -2,4 +2,19 @@
 
 using namespace std;
 
+class BinarySearchTree {
+    private:
+        struct TreeNode {
+            TreeNode* left;
+            int val;
+            TreeNode* right;
 
+            TreeNode(int value): val(value), left(nullptr), right(nullptr) {}
+        };
+        TreeNode* root;
+    
+    public:
+        BinarySearchTree(): root(nullptr) {}
+
+        
+};
